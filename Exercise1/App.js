ce55d319729fb2cb139647d137eb5aac8fb51445
OMikk <ossimikkonen@hotmail.com>
2020-04-04 15:10:14 +0300

    ex1

--- Exercise1/App.js
+++ Exercise1/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Button, TextInput, View, StyleSheet } from "react-native";
-import styles from "./styles.js";
 
 export default class App extends Component {
   constructor(props) {
@@ -30,14 +29,14 @@ export default class App extends Component {
         <TextInput
           style={styles.numbox}
           placeholder="Num1"
-          onChangeText={Num1 => {
+          onChangeText={(Num1) => {
             this.setState({ Num1 });
           }}
         />
         <TextInput
           style={styles.numbox}
           placeholder="Num2"
-          onChangeText={Num2 => {
+          onChangeText={(Num2) => {
             this.setState({ Num2 });
           }}
         />
@@ -53,3 +52,35 @@ export default class App extends Component {
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  button1: {
+    width: "10%",
+    height: 40,
+    marginLeft: "50%",
+    paddingRight: "0.5%",
+  },
+  button2: {
+    width: "10%",
+    height: 40,
+    marginRight: "50%",
+    paddingLeft: "0.5%",
+  },
+  numbox: {
+    borderWidth: 1,
+    margin: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "50%",
+    marginLeft: "25%",
+  },
+  result: {
+    margin: 5,
+    textAlign: "center",
+    fontSize: 24,
+  },
+});
